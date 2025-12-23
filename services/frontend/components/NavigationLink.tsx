532579931ba77f9ef575e430b1a4cf07a125f9d6
Quentin Derimais <quentin.derimais@gmail.com>
2025-12-23 15:28:42 +0100

    Style : Creating navigation link component

---
+++ services/frontend/components/NavigationLink.tsx
@@ -0,0 +1,15 @@
+import type {JSX} from "solid-js"
+
+interface NavigationLinksProps extends JSX.HTMLAttributes<HTMLAnchorElement>{
+    children: JSX.Element
+    target: string,
+}
+
+export default function NavigationLink(props: NavigationLinksProps) {
+
+  return (
+    <>
+        <a href={props.target} class=" font-title font-bold text-light text-3xl hover:text-primary-dark">{props.children}</a>
+    </>
+  );
+}
