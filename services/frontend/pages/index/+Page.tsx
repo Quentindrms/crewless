3eccad6e9437d1def671738b80035606c2ffd08a
Quentin Derimais <quentin.derimais@gmail.com>
2025-12-23 11:43:28 +0100

    Chore : adding layout and test css variables

---
+++ services/frontend/pages/index/+Page.tsx
@@ -0,0 +1,10 @@
+
+export default function Index(){
+
+    return(
+        <>
+            <h1 class="text-primary-light font-title font-bold text-6xl">Titre</h1>
+            <h2 class="font-title text-6xl">Titre 2</h2>
+        </>
+    )
+}
