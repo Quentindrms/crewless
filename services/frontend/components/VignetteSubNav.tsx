04c07200472e86dc4cfff917d36f695b7eefb85d
Quentin Derimais <quentin.derimais@gmail.com>
2025-12-23 16:20:48 +0100

    Style : changing name from Vignette to VignetteSubNav

--- services/frontend/components/VignetteSubNav.tsx
+++ services/frontend/components/VignetteSubNav.tsx
@@ -1,7 +1,7 @@
 import clsx from "clsx";
 import type { JSX } from "solid-js";
 
-interface VignetteProps extends JSX.HTMLAttributes<HTMLAnchorElement> {
+interface VignetteSubNavProps extends JSX.HTMLAttributes<HTMLAnchorElement> {
   children: JSX.Element;
   target: string,
 }
@@ -9,6 +9,6 @@ interface VignetteProps extends JSX.HTMLAttributes<HTMLAnchorElement> {
 const globalClasses =
   "cursor-pointer p-3 w-fit h-fit text-center font-medium text-md text-light bg-primary-light rounded-xl hover:bg-primary-dark";
 
-export default function Vignette(props: VignetteProps) {
+export default function VignetteSubNav(props: VignetteSubNavProps) {
   return <a class={clsx([globalClasses])}>{props.children}</a>;
 }
