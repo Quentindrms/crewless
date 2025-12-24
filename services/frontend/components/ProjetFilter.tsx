2291908f2227c29473c55701ee2a945e9bf733a3
Quentin Derimais <quentin.derimais@gmail.com>
2025-12-24 18:57:12 +0100

    Feat : add new component for filtering projects

---
+++ services/frontend/components/ProjetFilter.tsx
@@ -0,0 +1,17 @@
+import { For } from "solid-js";
+import VignetteSubNav from "./VignetteSubNav";
+
+const labels = ["Idées", "En cours", "Planifié", "En pause", "Terminé", "Abandonné"];
+
+export default function ProjetFilter(){
+
+    return(
+        <div class="grid grid-cols-3 grid-rows-2 w-xs lg:w-lg items-center justify-items-center gap-2">
+            <For each={labels}>
+                {(label) =>(
+                    <VignetteSubNav target="#">{label}</VignetteSubNav>
+                )}
+            </For>
+        </div>
+    )
+}
