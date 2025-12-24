cdce799c9f75d17d55e70bbcdc5a6297c40c856c
Quentin Derimais <quentin.derimais@gmail.com>
2025-12-24 18:26:35 +0100

    Feature : add overview for link related to a task

---
+++ services/frontend/components/VignetteLien.tsx
@@ -0,0 +1,25 @@
+import clsx from "clsx";
+import Heading from "./Heading";
+import Paragraph from "./Paragraph";
+
+interface VignetteLiensProps {
+    title: string,
+    target: string,
+    description: string,
+}
+
+const globalClasses = "flex flex-col gap-2 w-xs border border-secondary rounded-xl p-2"
+const desktopClasses = "lg:w-lg"
+
+export default function VignetteLien(props: VignetteLiensProps) {
+
+    return (
+        <>
+            <div class={clsx([globalClasses, desktopClasses])}>
+                <Heading component="h3" level={3} bold="bold">{props.title}</Heading>
+                <Paragraph>{props.description}</Paragraph>
+                <Paragraph class=""><a href="" class="text-primary-light hover:text-primary-dark text-lg">{props.target}</a></Paragraph>
+            </div>
+        </>
+    )
+}
