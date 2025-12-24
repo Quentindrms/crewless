6225d4459ea60870b7474274eacac175b13c4d4c
Quentin Derimais <quentin.derimais@gmail.com>
2025-12-24 18:19:20 +0100

    Fix : Add props values into component and display them

--- services/frontend/components/Projet.tsx
+++ services/frontend/components/Projet.tsx
@@ -20,14 +20,13 @@ export default function Projet(props: ProjetProps) {
     return (
         <div class={clsx([defaultClasses, desktopClasses])} id="projet">
             <div>
-                <Heading component="h3" level={3} bold="bold">Nom de la tâche</Heading>
-                <Paragraph boldValue="normal" class="text-sm xl:text-base w-fit h-35 text-justify p-5 overflow-scroll">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rutrum semper eleifend. Ut id nisi at est accumsan pulvinar eu nec tellus. Aenean eget faucibus ante. Suspendisse ultrices nulla id imperdiet porta. Curabitur cursus vitae leo quis elementum. Suspendisse quis malesuada nisi, id pellentesque ligula. Nullam et sollicitudin eros, non rutrum nisi. Vestibulum id sagittis erat, et interdum est. Integer non enim eget elit eleifend commodo in vel ipsum. Duis vitae commodo urna. Nam posuere libero ac turpis pharetra, in accumsan massa tincidunt.
-                </Paragraph>
+                <Heading component="h3" level={3} bold="bold">{props.name}</Heading>
+                <Paragraph boldValue="normal" class="w-fit max-h-35 text-justify p-5 overflow-scroll">{props.description}</Paragraph>
             </div>
             <div class="flex justify-between p-2 items-center">
                 <div>
-                    <Paragraph boldValue="medium" class="italic">En cours</Paragraph>
-                    <Paragraph boldValue="normal" class="text-lg">Idée</Paragraph>
+                    <Paragraph boldValue="medium" class="italic">{props.statut}</Paragraph>
+                    <Paragraph boldValue="normal" class="text-lg">{props.type}</Paragraph>
                 </div>
                 <div>
                     <Paragraph boldValue="bold" id="tempsPassé">
