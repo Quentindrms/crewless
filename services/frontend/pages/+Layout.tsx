2c0456f8d2ecc93c2c603a31df4e58c23ee8d194
Quentin Derimais <quentin.derimais@gmail.com>
2025-12-24 14:49:14 +0100

    Fix : correcting background display

--- services/frontend/pages/+Layout.tsx
+++ services/frontend/pages/+Layout.tsx
@@ -4,7 +4,7 @@ import "./tailwind.css";
 
 export default function Layout(props:{children?: JSX.Element}){
 return(
-    <div class="flex flex-col h-screen grow bg-background-dark">
+    <div class="flex flex-col grow bg-background-dark">
         {props.children}
     </div>
 )
