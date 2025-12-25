b7cb6c29cab09bdb29dd9d0e32e236fa4c2aa092
Quentin Derimais <quentin.derimais@gmail.com>
2025-12-25 12:44:02 +0100

    Feat : Integrate header and navigation into layout

--- services/frontend/pages/+Layout.tsx
+++ services/frontend/pages/+Layout.tsx
@@ -1,11 +1,28 @@
 
-import type {JSX} from "solid-js";
+import type { JSX } from "solid-js";
 import "./tailwind.css";
+import SidebarNavigation from "../components/navigation/SidebarNavigation";
+import Header from "../components/Header";
 
-export default function Layout(props:{children?: JSX.Element}){
-return(
-    <div class="flex flex-col grow bg-background-dark">
-        {props.children}
-    </div>
-)
+export default function Layout(props: { children?: JSX.Element }) {
+    return (
+        <div>
+            <Header />
+            <div class="flex flex-row">
+                <Sidebar />
+                <div class="flex flex-col grow bg-background-dark">
+                    {props.children}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function Sidebar() {
+
+    return (
+        <div class="w-50 h-screen hidden lg:block">
+            <SidebarNavigation />
+        </div>
+    )
 }
