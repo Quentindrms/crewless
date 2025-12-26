f8cd66cf2b581725834c216ba895cdb5a0fe162d
Quentin Derimais <quentin.derimais@gmail.com>
2025-12-26 20:47:03 +0100

    Style : apply prettier on files

--- services/frontend/components/forms/FormInput.tsx
+++ services/frontend/components/forms/FormInput.tsx
@@ -1,21 +1,37 @@
 import type { JSX } from "solid-js";
 
-type FormInputType = "text" | "email" | "phone" | "password" | "number" | "date" | "time";
+type FormInputType =
+  | "text"
+  | "email"
+  | "phone"
+  | "password"
+  | "number"
+  | "date"
+  | "time";
 
 interface FormInputProps {
-    children: JSX.Element,
-    inputType?: FormInputType
-    placeholder?: string,
-    id?: number,
-    name?: string,
+  children: JSX.Element;
+  inputType?: FormInputType;
+  placeholder?: string;
+  id?: number;
+  name?: string;
 }
 
-export default function FormInput(props:FormInputProps) {
-
-    return (
-        <div class="flex flex-col">
-            <label class="font-title text-light text-3xl" for={`-${props.name}-${props.id?.toString()}`}>{props.children}</label>
-            <input type={props.inputType ?? "text"} id={`-${props.name}-${props.id?.toString()}`}placeholder="Test" class="bg-background-light font-main text-dark w-2xs lg:w-xs"></input>
-        </div>
-    )
+export default function FormInput(props: FormInputProps) {
+  return (
+    <div class="flex flex-col">
+      <label
+        class="font-title text-light text-3xl"
+        for={`-${props.name}-${props.id?.toString()}`}
+      >
+        {props.children}
+      </label>
+      <input
+        type={props.inputType ?? "text"}
+        id={`-${props.name}-${props.id?.toString()}`}
+        placeholder="Test"
+        class="bg-background-light font-main text-dark w-2xs lg:w-xs"
+      ></input>
+    </div>
+  );
 }
--- services/frontend/components/forms/FormSelect.tsx
+++ services/frontend/components/forms/FormSelect.tsx
@@ -1,30 +1,26 @@
-import {For, type JSX} from "solid-js";
+import { For, type JSX } from "solid-js";
 
 type OptionType = {
-    name: string,
-    value: string,
-}
+  name: string;
+  value: string;
+};
 
 interface FormSelectProps {
-    children: JSX.Element,
-    name: string, 
-    options: OptionType[],
+  children: JSX.Element;
+  name: string;
+  options: OptionType[];
 }
 
-export default function FormSelect(props:FormSelectProps){
-
-
-    return(
-        <>
-        <label class="font-title text-light text-3xl">{props.children}</label>
-            <select class="bg-light font-title w-2xs lg:w-xs">
-                <option value="">Choisir une option</option>
-                <For each={props.options}>
-                    {(option) => (
-                        <option value={option.value}>{option.name}</option>
-                    )}
-                </For>
-            </select>
-        </>
-    )
+export default function FormSelect(props: FormSelectProps) {
+  return (
+    <>
+      <label class="font-title text-light text-3xl">{props.children}</label>
+      <select class="bg-light font-title w-2xs lg:w-xs">
+        <option value="">Choisir une option</option>
+        <For each={props.options}>
+          {(option) => <option value={option.value}>{option.name}</option>}
+        </For>
+      </select>
+    </>
+  );
 }
