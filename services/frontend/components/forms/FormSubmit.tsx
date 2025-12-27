dbd5b77b54ff7c8512bfc90afeb66edd6e653cad
Quentin Derimais <quentin.derimais@gmail.com>
2025-12-27 11:25:51 +0100

    Feature : add component for submitting forms

---
+++ services/frontend/components/forms/FormSubmit.tsx
@@ -0,0 +1,14 @@
+import type {JSX} from "solid-js";
+
+interface FormSubmitProps{
+    value: string,
+}
+
+export default function FormSubmit(props:FormSubmitProps){
+
+    return(
+        <>
+            <input class="bg-accent-light rounded-xl w-fit p-3 text-light text-title font-bold hover:bg-accent-dark" type="submit" value={props.value}></input>
+        </>
+    )
+}
