8e8a394afd09233ca7a57b1b1dcdf01151748796
Quentin Derimais <quentin.derimais@gmail.com>
2025-12-27 11:35:49 +0100

    Style : apply prettier

--- services/frontend/components/forms/FormSubmit.tsx
+++ services/frontend/components/forms/FormSubmit.tsx
@@ -1,14 +1,17 @@
-import type {JSX} from "solid-js";
+import type { JSX } from "solid-js";
 
-interface FormSubmitProps{
-    value: string,
+interface FormSubmitProps {
+  value: string;
 }
 
-export default function FormSubmit(props:FormSubmitProps){
-
-    return(
-        <>
-            <input class="bg-accent-light rounded-xl w-fit p-3 text-light text-title font-bold hover:bg-accent-dark" type="submit" value={props.value}></input>
-        </>
-    )
+export default function FormSubmit(props: FormSubmitProps) {
+  return (
+    <>
+      <input
+        class="bg-accent-light rounded-xl w-fit p-3 text-light text-title font-bold hover:bg-accent-dark"
+        type="submit"
+        value={props.value}
+      ></input>
+    </>
+  );
 }
