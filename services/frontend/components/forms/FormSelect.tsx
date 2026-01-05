51ed653a4a83639654554ff2b99836fd584999c4
Quentin Derimais <quentin.derimais@gmail.com>
2026-01-05 15:29:58 +0100

    Feat : add onChange property to catch changes on a select and send him to formData

--- services/frontend/components/forms/FormSelect.tsx
+++ services/frontend/components/forms/FormSelect.tsx
@@ -9,13 +9,14 @@ interface FormSelectProps {
   children: JSX.Element;
   name: string;
   options: OptionType[];
+  onChange: (e:Event) => void,
 }
 
 export default function FormSelect(props: FormSelectProps) {
   return (
     <>
       <label class="font-title text-light text-3xl">{props.children}</label>
-      <select class="bg-light font-title w-2xs lg:w-xs">
+      <select class="bg-light font-title w-2xs lg:w-xs" onChange={props.onChange}>
         <option value="">Choisir une option</option>
         <For each={props.options}>
           {(option) => <option value={option.value}>{option.name}</option>}
