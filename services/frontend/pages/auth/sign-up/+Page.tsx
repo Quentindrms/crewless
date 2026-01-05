2ea8cbdbc782207f61f838b8304954f493d4e09a
Quentin Derimais <quentin.derimais@gmail.com>
2026-01-05 12:05:10 +0100

    Feat : Create complete form for connexion

--- services/frontend/pages/auth/sign-up/+Page.tsx
+++ services/frontend/pages/auth/sign-up/+Page.tsx
@@ -1,10 +1,40 @@
+import { createSignal } from "solid-js"
 import "../../tailwind.css"
+import { SignUpFormType } from "../../../types/forms/AuthFormType"
+import Heading from "../../../components/Heading"
+import FormInput from "../../../components/forms/FormInput"
+import FormSubmit from "../../../components/forms/FormSubmit"
 
 export default function Signup(){
 
+    const [formData, setFormData] = createSignal<SignUpFormType>({
+        mail: '',
+        password: '',
+    })
+
+    const handleInputChange = (field: keyof SignUpFormType) => (e:Event) => {
+        const target = e.target as HTMLInputElement;
+        setFormData(prev => ({
+            ...prev,
+            [field]: target.value
+        }))
+        console.log(formData())
+    }
+
+    function handleSubmit(e:Event){
+        e.preventDefault();
+        console.log(formData())
+    }
+
+
     return (
-        <>
-        
-        </>
+        <div class="flex flex-col items-center justify-center min-h-screen">
+            <Heading level={2} component="h2">Connexion</Heading>
+            <form class="flex flex-col items-center gap-2" onSubmit={(e:Event) => handleSubmit(e)}>
+                <FormInput inputType="email" onChange={handleInputChange("mail")}>Adresse mail</FormInput>
+                <FormInput inputType="password" onChange={handleInputChange('password')}>Mot de passe</FormInput>
+                <FormSubmit value="Connexion"></FormSubmit>
+            </form>
+        </div>
     )
 }
