fc40a780b3a4b8d171e25ed8fdcc485efdaec46e
Quentin Derimais <quentin.derimais@gmail.com>
2026-01-06 16:12:31 +0100

    Feats : Connect all modules to app module

--- services/backend/src/app.module.ts
+++ services/backend/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProjectsModule } from './projects/project.module';
+import { NotesModules } from './notes/notes.module';
+import { LinksModules } from './links/links.module';
+import { TasksModule } from './tasks/tasks.module';
+import { CategoriesController } from './categories/categories.controller';
 
 @Module({
-  imports: [],
+  imports: [UsersModule,
+    ProjectsModule,
+    NotesModules,
+    LinksModules,
+    TasksModule,
+    CategoriesController],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
