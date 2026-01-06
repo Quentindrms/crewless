a99b7b2d39e3546cf18a06c36bf8aef0fae2df8b
Quentin Derimais <quentin.derimais@gmail.com>
2026-01-06 15:54:26 +0100

    Feats : Creates route for notes

---
+++ services/backend/src/notes/notes.controller.ts
@@ -0,0 +1,11 @@
+import {Controller, Get} from '@nestjs/common'
+import { NotesService } from './notes.service'
+
+@Controller('notes')
+export class NotesController {
+
+    @Get()
+    findAll():string{
+        return 'notes'
+    }
+}
