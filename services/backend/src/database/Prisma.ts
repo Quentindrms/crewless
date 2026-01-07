b3fe01070271a6e6609f060ed62c7d1dc213e792
Quentin Derimais <quentin.derimais@gmail.com>
2026-01-07 14:34:41 +0100

    Feat : Create a singleton for connection pool, allow to use Prisma

---
+++ services/backend/src/database/Prisma.ts
@@ -0,0 +1,22 @@
+import 'dotenv/config';
+import { PrismaClient } from 'generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+class Prisma {
+
+    private static instance: PrismaClient;
+
+    public static getInstance(): PrismaClient{
+        const connectionString = `${process.env.DATABASE_URL}` || undefined;
+        if(!Prisma.instance){
+            const adapter = new PrismaPg({connectionString})
+            Prisma.instance = new PrismaClient({
+                adapter: adapter,
+            })
+        }
+        return Prisma.instance;
+    }
+}
+
+const prisma = Prisma.getInstance();
+export default prisma;
