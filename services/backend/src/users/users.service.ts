2c87919a6f34854855593578f8caab22a59fa662
Quentin Derimais <quentin.derimais@gmail.com>
2026-01-08 15:26:53 +0100

    Feats : creates routes for creating user and hash password with argon2

--- services/backend/src/users/users.service.ts
+++ services/backend/src/users/users.service.ts
@@ -1,43 +1,52 @@
 import { Injectable } from "@nestjs/common";
 import { User } from "generated/prisma/browser";
+import { throwError } from "rxjs";
 import prisma from "src/database/Prisma";
 
 @Injectable()
 export class UsersService {
-    
-async readUser(uuid: string):Promise<User|null>{
-    const data = await prisma.user.findUnique({
-        where:{
-            idUser: uuid,
-        }
-    })
-    return data;
-}
 
-async editUser(uuid:string):Promise<User|null>{
-    const data = await prisma.user.update({
-        where:{
-            idUser: uuid,
-        },
-        data:{
+    async readUser(uuid: string): Promise<User | null> {
+        const data = await prisma.user.findUnique({
+            where: {
+                idUser: uuid,
+            }
+        })
+        return data;
+    }
 
-        }
-    })
-    return data;
-}
+    async editUser(uuid: string): Promise<User | null> {
+        const data = await prisma.user.update({
+            where: {
+                idUser: uuid,
+            },
+            data: {
 
-async addUser(lastName: string, firstName: string, username: string, mail:string, password: string):Promise<User|null>{
-    const data = await prisma.user.create({
-        data:{
-            lastName: lastName,
-            firstName: firstName,
-            username: username,
-            mail: mail,
-            hashedPassword: password,
+            }
+        })
+        return data;
+    }
+
+    async addUser(lastName: string, firstName: string, username: string, mail: string, password: string): Promise<User | null> {
+        const argon2 = require('argon2');
+        try {
+            const hashPassword = await argon2.hash(password)
+
+            const data = await prisma.user.create({
+                data: {
+                    lastName: lastName,
+                    firstName: firstName,
+                    username: username,
+                    mail: mail,
+                    hashedPassword: hashPassword,
+                }
+            })
+            return data;
+        } catch (err) {
+            console.error(err);
         }
-    })
-    return data;
-}
+        return null;
+    }
 
 
 
