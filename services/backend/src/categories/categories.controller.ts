7c946da90a99c17b30f961d4868a167e7beef2fe
Quentin Derimais <quentin.derimais@gmail.com>
2026-01-08 11:27:01 +0100

    Feats : create routes to browse and read categories

--- services/backend/src/categories/categories.controller.ts
+++ services/backend/src/categories/categories.controller.ts
@@ -1,9 +1,18 @@
-import {Controller, Get, Post} from "@nestjs/common";
+import {Controller, Get, Param, ParseIntPipe} from "@nestjs/common";
+import { CategoriesService } from "./categories.service";
+import { Category } from "generated/prisma/browser";
 
 @Controller('categories')
 export class CategoriesController{
-    @Get()
-    findAll(): string{
-        return 'categories'
+    constructor(private readonly categoriesService: CategoriesService){}
+
+    @Get('list')
+    findAll(): Promise<Category[]|null>{
+        return this.categoriesService.browseCategories();
+    }
+
+    @Get('read/:id')
+    findOne(@Param('id', ParseIntPipe) id:number): Promise<Category|null>{
+        return this.categoriesService.readCategory(id);
     }
 }
--- services/backend/src/categories/categories.service.ts
+++ services/backend/src/categories/categories.service.ts
@@ -1,6 +1,36 @@
 import { Injectable } from "@nestjs/common";
+import { Category } from "generated/prisma/client";
+import prisma from "src/database/Prisma";
 
 @Injectable()
 export class CategoriesService{
-    
+
+    async browseCategories():Promise<Category[]|null>{
+        const data = await prisma.category.findMany();
+        return data;
+    }
+
+    async readCategory(id:number):Promise<Category|null>{
+        const data = await prisma.category.findUnique({
+            where: {
+                idCategory: id,
+            }
+        });
+        return data;
+    }
+
+    async editCategory(id: number){
+
+    }
+
+    async addCategory(){
+
+    }
+
+    async deleteCategory(id: number){
+
+    }
+
+
+
 }
