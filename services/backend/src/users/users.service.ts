f80030db9289a32575df8054ef79e09778e3f523
Quentin Derimais <quentin.derimais@gmail.com>
2026-01-08 14:01:09 +0100

    Feat : Create route for fiding user

--- services/backend/src/users/users.service.ts
+++ services/backend/src/users/users.service.ts
@@ -1,6 +1,31 @@
 import { Injectable } from "@nestjs/common";
+import { User } from "generated/prisma/browser";
+import prisma from "src/database/Prisma";
 
 @Injectable()
 export class UsersService {
     
+async redUser(uuid: string):Promise<User|null>{
+    const data = await prisma.user.findUnique({
+        where:{
+            idUser: uuid,
+        }
+    })
+    return data;
+}
+
+async editUser(uuid:string):Promise<User|null>{
+    const data = await prisma.user.update({
+        where:{
+            idUser: uuid,
+        },
+        data:{
+
+        }
+    })
+    return data;
+}
+
+
+
 }
