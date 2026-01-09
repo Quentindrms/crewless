0b2f7fae5a7d76363e243ea4f0266a1b54ce6f89
Quentin Derimais <quentin.derimais@gmail.com>
2026-01-09 11:40:10 +0100

    Feats : Create services for notes

--- services/backend/src/notes/notes.service.ts
+++ services/backend/src/notes/notes.service.ts
@@ -1,6 +1,49 @@
 import { Injectable } from "@nestjs/common";
+import { Notes } from "generated/prisma/browser";
+import { not } from "rxjs/internal/util/not";
+import prisma from "src/database/Prisma";
 
 @Injectable()
 export class NotesService {
     
+    browseProjectNotes(userUuid: string, projectUuid: string): Promise<Notes[]|null>{
+        return prisma.notes.findMany({
+            where:{
+                projectId: projectUuid,
+            }
+        })
+    }
+
+    readNote(noteId: number): Promise<Notes|null>{
+        return prisma.notes.findFirst({
+            where:{
+                idNotes: noteId,
+            }
+        })
+    }
+
+    editNote(noteId:number, noteContent:string): Promise<Notes|null>{
+        return prisma.notes.update({
+            where:{
+                idNotes: noteId
+            },
+            data:{
+                content: noteContent,
+            }
+        })
+    }
+
+    addNote(noteName:string, noteContent:string, projectUuid: string): Promise<Notes|null>{
+        return prisma.notes.create({
+            data:{
+                idNotes: 0,
+                name: noteName,
+                content: noteContent,
+                updateAt: Date.now().toString(),
+                description: '',
+                projectId: projectUuid,
+            }
+        })
+    }
+
 }
